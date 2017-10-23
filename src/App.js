58c7cebd794a0b97cf05d27536608155af22b4b4
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-10-23 22:30:17 +0200

    added projects component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business website',
+          caytegory: 'web design'
+        },
+        {
+          title: 'Social website',
+          caytegory: 'mobile developement'
+        },
+        {
+          title: 'Ecom website',
+          caytegory: 'web design'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Projects />
+        <Projects projects = {this.state.projects}/>
       </div>
     );
   }
