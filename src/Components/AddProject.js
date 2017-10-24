eee73e403317d7d5c2e7420665f440b6db5336d5
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-10-24 10:58:59 +0200

    added movie list

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+  static defaultProps = {
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
   render() {
 
     return (
       <div>
         <h3>Add Project</h3>
+        <form>
+          <div>
+            <label>Title</label><br />
+            <input type="text" ref="title" />
+            <select ref="category">
+
+            </select>
+          </div>
+          <div>
+            <label>Category</label><br />
+
+          </div>
+        </form>
       </div>
     );
   }
